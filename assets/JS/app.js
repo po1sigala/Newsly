ea1dc3fde5f9c3996a492de7d2f587c65d2f9d3c
Your Name <alexsigala87@gmail.com>
2019-04-23 21:03:55 -0700

    added outline of what needs to be done in each file

---
+++ assets/JS/app.js
@@ -0,0 +1,26 @@
+//documentready the thing
+/*fucntions section
+
+for dynamically created results
+    dynamically create whatever the card class with the header and footer
+    give it an img attr 
+    give it a button on the bottom
+    make the button link to the article
+    give the button a special id for the hover funciton later
+    set the header to the article title
+    set the footer to some text from the article
+hover
+    when hovering over buttons in the results section it shoudl preview the site
+    hovering over any link should change the color
+    hovering over the ticker shoudl sop the ticker
+    hovering over the carosel should pause the carosel
+    hovering over the arrows on the carosel should accent their color
+clicks
+    clicking on a result button should take you to the article
+    clicking the search button should run our dynamically created button fucntion
+    clicking the check boxes should add them to some array that will be used for the API
+API
+    twitter search API for ticker
+    twitter share API fro creating tweets about what the user found
+    NewsAPI to query sites
+*/ 
