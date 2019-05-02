388e8c796bb8be1dfd071df0c5d91c04993c8fbc
Your Name <alexsigala87@gmail.com>
2019-05-01 18:04:04 -0700

    updatingJS

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,92 +1,136 @@
 //documentready the thing
+
+
+
+
 //below are our global variables
 //storing the sources we want to search for
 // var search = ""; //user search will be a "this.val" object i can call once the html is set up for the search bar
 
 
 //trying to dynamically create custom col breaks so if its on card col-md-12
-// var numOfCards=0;
+var numOfCards=0;
 // var numColumns= 12;
 // if (numOfCards=1) 
 
+$(document).ready(function(){
+    $("#lastSearch").text(localStorage.getItem("lastSearch"));
+
+    // on ready we want any last searched terms to be displayed from local storage
+
 $("#searchButton").on("click",function(){
-//----------------------update the above class------------------------------------------------
-console.log("clicked")
-$(".cardRow").empty();//clear out the example cards
-   var search= $(".searchBox").val();//set the search variable to the text of the search box
+
+    $(".cardRow").empty();//clear out the example cards
+    var search= $(".searchBox").val();//deaclare a search variable set to the text of the search box
 
 
-    // get all checked boxes
-   var checkedBoxes=$("input:checked").map(function(){
-       console.log(this.id);
-       return this.id;
-    
-   })
-   .get();
-   console.log(checkedBoxes);
-    // get all ids from checked boxes
-   
-    // user all ids as array to for loop
-    // look at jQuery map method
-    
-
+        // get all boxes our user checked and put them into and array
+    var checkedBoxes=$("input:checked").map(function(){
+        return this.id;//our arrary is composed of the id's of the buttons. these ids match the newsAPI id search keys
+        
+    })
+    .get();//turns our jQuery array into a javascript array
+        
+    //The nature of the sorting of this API's resonses means we have to use a for loop and run multiple queries if we want to ensure each source is queried for the input
     for(i=0;i<checkedBoxes.length;i++){
-        var site = checkedBoxes[i];//we are setting var site to the element at index i
-        // numbOfCards++
-        searchSites(search, site)//we are running the searchsites function with the result of selectedsites{i}
+           var site = checkedBoxes[i];//we are setting var site to the element at index i
+            
+            searchSites(search, site)//we are running the searchsites function with the result of selectedsites{i}
 
-    }
-    // searchSites(selectedSites);//if working correctly search sites will run with a source parameter of i aka which site the user wants to check
+        }
+        // searchSites(selectedSites);//if working correctly search sites will run with a source parameter of i aka which site the user wants to check
+
+
+        //below we want to add searches to local storage for next time the user loads
+        localStorage.clear();
+        localStorage.setItem("lastSearch",search);
+        $("#lastSearch").text(localStorage.getItem("lastSearch"));
+    
+    searchTwitter(search);
 
 })
 var searchSites= function(search, source){
-    //were gonna need two query urls i think that will be called in two different ajax requests
-    //the type of news we're looking for
-    //takes the array and joins it and seperates by a comma makes an API request from all the sources
-    // source=source.join(',')
-    
-    console.log(search);
-    console.log(source);
-    //source like what kind of 
     var newsQueryURL = 
-    // " 
-                        'https://newsapi.org/v2/everything?q='
-                        + search +'&sources='
-                        + source 
-                        +"&pageSize=1"
-                        + '&apiKey=2328e350ebab4672a9dfa7ce0fdddacd';
-// this will query the news API for our users searchinput and whatever source they selected
+                        'https://newsapi.org/v2/everything?q='//queries all articles
+                        + search +'&sources='//searches articles above for the text the user put in the text box and adds a input for specific sites
+                        + source //here we are populating the id's the API takes from our array. the id's of the checked boxes match the API source ID's
+                        +"&pageSize=1"//I am just taking the firts response that oure API gives us
+                        + '&apiKey=2328e350ebab4672a9dfa7ce0fdddacd';//my API key
+
         console.log(newsQueryURL); //debug to make sure the url is coming out ok
     $.ajax({
         url: newsQueryURL,
         method: "GET"
     })
         .then(function(response){
-            console.log(response);
-            
-            //create our card div with the image divs in it and append it to the card row
-            var newimage= $("<img>").attr("src", response.articles[0].urlToImage );
-            console.log(newimage);
-            var newDiv = $("<div>").addClass("post-image")
-            newDiv.append(newimage);
-            var cardDiv= $("<div>").addClass("card");
-            cardDiv.append(newDiv);
+             numOfCards++;
+            //goal of this block is to create our card div with the image divs in it and append it to the card row
 
-            var responsiveDiv = $("<div>").addClass("col-sm-4");
-            responsiveDiv.append(cardDiv);
-            $(".cardRow").append(responsiveDiv);
-            
-            //create our news content div divs in it and append that to the card div
-           /* inside our carDiv I need news content div
-                inside news content I need span category, div post-meta, h2 post-header, and a p tag
-           */
-            var newsContentDiv=$("<div>").addClass("news-content")
-            
-                var categoryTitle= $("<span>").addClass("category").text(response.articles[0].source.name);
+                var newimage= $("<img>").attr("src", response.articles[0].urlToImage ).addClass("imgFit");
+                //variable that contains the cardimage
+                //added class here called imgFit which I hope targets the img and les me force it to fit the box via some css code
+
+                var newDiv = $("<div>").addClass("post-image")
+                //variable that contains the div that the card image goes in
+                newDiv.append(newimage);
+                //appending the new image into its div container
+                
                 var header = $("<h2>").addClass("post-header").text(response.articles[0].title);
                 var summary = $("<p>").text(response.articles[0].description);
-            newsContentDiv.append(categoryTitle).append(header).append(summary);
-            cardDiv.append(newsContentDiv);
+                var categoryTitle= $("<span>").addClass("category").text(response.articles[0].source.name);
+                var newsContentDiv=$("<div>").addClass("news-content")
+                newsContentDiv.append(categoryTitle).append(header).append(summary);
+                //
+                var cardDiv= $("<div>").addClass("card");
+                //variable that contains my div where all the query cards go
+                cardDiv.append(newDiv).append(newsContentDiv);
+                //append the dive witg the image of the query into the div that contains the whole card
+
+                var responsiveDiv = $("<div>").addClass("col-sm-4").addClass("cardNumber"+numOfCards);
+                console.log("this card has an id of cardNumber"+numOfCards)
+                //variable that holds the div which uses a bootstrap class to divide the 12 normal coumns by 4 to make 3 columns
+                responsiveDiv.append(cardDiv);
+
+
+
+
+                //append all the dynamically created card info into this div
+
+                // working on this following code to dynamiclaly create rows every three searches
+
+                numRows= Math.ceil(numOfCards/3);//calculate the rows needed to house all the cards
+
+
+                if (numRows>1){
+                    console.log("numrows is" + numRows);
+                    for (i=1;i<numRows;i++){//run a function the same number of times as i want rows created
+                        // var spacing= $("<br>");
+                        var newRow= $("<div>").addClass("cardrow"+numRows);//row 2 row 3 etc etc depending on how many sources chosen
+                        // newRow.append(spacing);
+                        console.log("looped")//should pop up one less time than the value of numRows
+                        $(".cardContainer").append(newRow);//put the new row in the card div
+                        
+                    }
+                }
+
+                $(".cardRow"+numRows).append(responsiveDiv);//otherwise just put it in the row already there if no new rows needed
+                }
+
+
+
+
+                $(".cardRow").append(responsiveDiv);
+                //append the card into the card section of the html
+                
+                //create our news content div divs in it and append that to the card div
+                /* inside our carDiv I need news content div
+                        inside news content I need span category, div post-meta, h2 post-header, and a p tag
+                */
+                
+                   
+                   
+                
+                
 
 
             /*
@@ -98,11 +142,29 @@ var searchSites= function(search, source){
             */
             
         })
-
-    var twitterQueryURL
-    $.ajax({})
     
 }
+
+// below is where my twitter request is happening it should run when the page refreshes and get the trending tweets globally
+// var searchTwitter= function(search){
+// var twitterQueryURL= "https://api.twitter.com/1.1/search/tweets.json?q="
+//                       +search;
+// $.ajax({
+//     url: twitterQueryURL,
+//     authorization: "2424115598-HJLl6fWq6zt2lSfsfDw9DNPH9IazrmgoJ7X8PC8",
+//     dataType:"jsonp",
+//     method: "GET"
+// })
+//     .then(function(response){
+        
+//         console.log("twitter function is running")(response);
+//     })
+// }
+// });
+
+//below is tester code im not sure if i want 
+
+ // 
 /*fucntions section
 for dynamically created results
     dynamically create whatever the card class with the header and footer
